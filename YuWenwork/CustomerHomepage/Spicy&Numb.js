b58858ed5f4602b38ef8054f2f3ad109ec2b08e7
OoiYuWen <S10273054@connect.np.edu.sg>
2026-02-04 19:10:51 +0800

    made changes to stalls.html, assignment.js, stalls.css, mala.css, spicy&numb.html and spicy&numb.js

--- YuWenwork/CustomerHomepage/Spicy&Numb.js
+++ YuWenwork/CustomerHomepage/Spicy&Numb.js
@@ -1,17 +1,114 @@
-const menuItems = [
-    {name: "Sausage", price:1.20, img: "images/sausage.jpg"},
-    {name: "Maggi Mee", price:2.00, img: "images/Maggi Mee.jpg"},
-    {name: "Lotus Root", price:1.50, img: "images/Lotus Root.jpg"},
-    {name: "Corn", price:1.50, img: "images/corn.webp"},
-    {name: "Potato", price:2.30, img: "images/Potato.jpg"},
-    {name: "Enoki Mushroom", price:1.50, img: "images/Enoki Mushrrom.jpg"}
-];
+const allStallMenu = {
+    "Spicy&Numb.html":[
+    {name: "Sausage", price:1.20, qty: 0,img: "https://static.vecteezy.com/system/resources/thumbnails/038/144/798/small_2x/ai-generated-tasty-goodness-sausages-isolated-on-white-background-photo.jpg"},
+    {name: "Maggi Mee", price:2.00, qty: 0,img: "https://m.media-amazon.com/images/I/61axCDVehZL.__AC_SY300_SX300_QL70_ML2_.jpg"},
+    {name: "Lotus Root", price:1.50, qty: 0,img: "https://www.thespruceeats.com/thmb/kpt7oOfWY_nYoHf4cHIQdzqcJdc=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/antpkr-ee31ee7c1ab5494480b004be970c6019.jpg"},
+    {name: "Corn", price:1.50, qty: 0,img: "https://th.bing.com/th/id/R.30db0cce9b92c24627620a5aa8eb9565?rik=SML5Z%2b0Tlyq9Lg&riu=http%3a%2f%2fpngimg.com%2fuploads%2fcorn%2fcorn_PNG5293.png&ehk=uFUcHy2Pca021l3tUTRY8Lw8bcN2olQCjcJm1hwCLBY%3d&risl=1&pid=ImgRaw&r=0"},
+    {name: "Potato", price:2.30, qty: 0,img: "https://th.bing.com/th/id/R.fb9c2752eb6f791a41d3336168edd187?rik=HeRvSZWC8Hi8cw&riu=http%3a%2f%2fwww.homifreez.com%2fpictures%2flist%2f385_detail.jpg%3f1392200807&ehk=k5%2beKOTYKH2aDkTKq4Z%2fXYRotDVAOkqb22fYMVfGH%2fY%3d&risl=&pid=ImgRaw&r=0"},
+    {name: "Enoki Mushroom", price:1.50, qty: 0,img: "https://th.bing.com/th/id/R.9a8a231f10a205a8d5a1d66377ac07e5?rik=OJXlcBUGbnz4sQ&riu=http%3a%2f%2fwww.chinasichuanfood.com%2fwp-content%2fuploads%2f2016%2f01%2fEnoki-mushrooms-steamed-with-garlic_.jpg&ehk=70FtGFp%2bTFye%2bAfhYc5bAMyuOLgz63xAcNjwEBmyVV8%3d&risl=&pid=ImgRaw&r=0"},
+    {name: "MeatBall", price: 1.00, qty: 0, img: "https://c8.alamy.com/comp/EATTXM/raw-uncooked-meatballs-on-a-isolated-white-background-EATTXM.jpg"},
+    {name: "Luncheon Meat", price: 1.10, qty: 0, img: "https://thefrenchgrocer.com/wp-content/uploads/2024/06/Luncheon_Meat-450x300.png"}
+    ],
+
+    "KingOfNoodle.html":[
+        {name: "Chicken Noodle Soup", price: 5.70, qty: 0, img: "https://tse2.mm.bing.net/th/id/OIP.5TRIHfaBWv-m8tjBSvLTEgHaKX?rs=1&pid=ImgDetMain&o=7&rm=3"},
+        {name: "Wonton Noodles (dry)", price: 6.00, qty: 0, img:"https://th.bing.com/th/id/R.e12eb2a96a34659455481f0e216c9fb3?rik=LLUOeiYGlF549A&riu=http%3a%2f%2fdelishar.com%2fwp-content%2fuploads%2f2013%2f10%2fwanton-noodle--1080x1386.jpg&ehk=FKemcJvDIbRxq0%2fU7jNfTqO9NfbP0c5qnvtMCsnQlP4%3d&risl=&pid=ImgRaw&r=0"},
+        {name: "Wonton Noodles (wet)", price: 6.50, qty: 0, img:"https://tasteasianfood.com/wp-content/uploads/2017/04/WT-square-bowl.jpg"},
+        {name: "Chow Mein", price: 5.90, qty: 0, img:"https://laurenslatest.com/wp-content/uploads/2021/04/chow-mein-12-scaled.jpg"},
+        {name: "Fried Bee Hoon", price: 5.40, qty: 0, img:"https://tse2.mm.bing.net/th/id/OIP.hsMHcSYDRlaiRnBuVllZGwHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"},
+        {name: "Meepok", price: 4.90, qty: 0, img:"https://eatbook.sg/wp-content/uploads/2020/01/Hosay-Mee-Pok-Mee-Pok-intro-shot.jpg"},
+        {name: "Laksa", price: 4.50, qty: 0, img:"https://www.chngkees.com.sg/image/catalog/project/recipes/Laksa.jpg"}
+    ],
+
+    "WesternDelights.html":[
+        {name: "Black Pepper Chicken Chop", price: 7.00, qty: 0, img:"https://chiefeater.com/wp-content/uploads/2024/10/lau_yew_sp_hawker_centre_oct2024_davidleesp_01.jpg"},
+        {name: "Carbonara Pasta", price: 5.90, qty: 0, img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7EhzGin5hXLm-1VWPj8ccNltsTPca5m7MNQAfLURWTV4NC_eaLn9J-DshUFippBO3nPDqqxD5vEcX6uzg7jGpkSjzUbiRGxc_gBeWD65ddXnDiASBzY1_Y0Mx0ISDsqhQOrCrPqC-AKg/w640-h640/Meet+4+Meat+Carbonara+1.jpg"},
+        {name: "Aglio Olio", price: 6.00, qty: 0, img:"https://therecipecritic.com/wp-content/uploads/2024/11/spaghetti-aglio-e-olio-2.jpg"},
+        {name: "Fried Chicken Wing", price: 6.00, qty: 0, img:"https://tse3.mm.bing.net/th/id/OIP.ypIDRyYFbMxvGzyKujNbsAHaJ4?rs=1&pid=ImgDetMain&o=7&rm=3"},
+        {name: "Fish & Chips", price: 7.00, qty: 0, img:"https://media.timeout.com/images/103696698/image.jpg"},
+        {name: "Mushroom Soup", price: 3.00, qty: 0, img:"https://bigoven-res.cloudinary.com/image/upload/t_recipe-1280/mushroom-soup-f2e36e.jpg"}
+    ],
+
+    "TteobokiHeaven.html":[
+        {name: "Tteobokki", price: 5.00, qty: 0, img:"https://www.mashed.com/img/gallery/what-is-tteokbokki-and-what-does-it-taste-like/l-intro-1617134138.jpg"},
+        {name: "Mandu (fried dumpling)", price: 2.00, qty: 0, img:"https://www.koreanbapsang.com/wp-content/uploads/2009/09/DSC_1191-e1538625826525.jpg"},
+        {name: "Korean Ramen", price: 5.00, qty: 0, img:"https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k/Photo/People/2022-05-AAPI-Month-Third-Culture/Raymum/K_AAPI_2022_Ramyun_003"},
+        {name: "Spicy Pork Bulgogi", price: 5.50, qty: 0, img:"https://tse3.mm.bing.net/th/id/OIP.VquC1yx898k-MXrWP0pYFgHaE8?rs=1&pid=ImgDetMain&o=7&rm=3"},
+        {name: "Kimchi Stew", price: 5.00, qty: 0, img:"https://tse4.mm.bing.net/th/id/OIP.AoWSXzmluxllf5KsWP_N6AHaLH?rs=1&pid=ImgDetMain&o=7&rm=3"},
+        {name: "Beef Hotplate", price: 5.90, qty: 0, img:"https://eatbook.sg/wp-content/uploads/2018/01/Daebak-Korean-Restaurant-Beef-Hot-Plate.jpg"}
+    ]
+};
+const filename = window.location.pathname.split("/").pop(); // This line finds out the name of the HTML file currently being viewed.
+//window.location.pathname: Gets the full path (e.g., /folder/subfolder/WesternDelights.html).
+//.split("/"): Breaks that path into a list whenever there is a slash: ["", "folder", "subfolder", "WesternDelights.html"].
+const currentPage = filename ==="" ? "Spicy&Numb.html":filename
+const menuItems = allStallMenu[currentPage] || allStallMenu["Spicy&Numb.html"] //This line picks the data that matches the filename.
+// allStallMenu[currentPage]: It looks into your big "dictionary" of food. If currentPage is "WesternDelights.html", it grabs the Western food list.
 
 const itemsGrid = document.getElementById("itemsGrid");
-menuItems.forEach(item  => {
-    const card = document.createElement("div");
-    card.classList.add("item-card");
+const totalDisplay = document.getElementById("totalDisplay");
 
-    card.innerHTML = `
-    <img src="${item.img}`
-})
+const backButton = document.querySelector('.back-nav');
+if (backButton){
+    backButton.style.cursor = "pointer";
+    backButton.addEventListener('click', () =>{
+        window.history.back();
+    })
+}
+
+function renderMenu(){
+    if (!itemsGrid) return; // safety check
+    itemsGrid.innerHTML = ""; // clear grid
+    let grandTotal = 0;
+    
+    menuItems.forEach((item, index)=> {
+        grandTotal += item.price*item.qty
+        // creates a new <div> in memory, but it's not yet part of the page
+        const card = document.createElement("div");
+        card.classList.add("item-card");
+
+        // Logic for the reveal: 
+        // if qty is 0, only show plus symbol
+        // if qty > 0, show the minus, number and plus sign 
+        let controllayout = "";
+        if (item.qty === 0){
+            controllayout = `
+            <div class="control">
+                 <span class="qty-circle" onclick="changeQty(${index}, 1)">+</span>
+            </div>
+            `
+        } 
+        else {
+            controllayout = `
+            <div class="control">
+            <span class="minus-symbol" onclick="changeQty(${index}, -1)">-</span>
+            <span class="qty-number">${item.qty}</span> 
+            <span class="qty-circle" onclick="changeQty(${index}, 1)">+</span>
+            </div>
+            `;
+        }
+
+        //fills that <div> with item's html (image, name, price)
+        card.innerHTML = `
+            <img src="${item.img}" alt="${item.name}" class="menu-img">
+            <h3>${item.name}</h3>
+            <p class="price">$${item.price.toFixed(2)}</p>
+            ${controllayout}
+        `;
+        itemsGrid.appendChild(card);
+        // actually inserts that <div> into the <div id="itemsGrid"></div> in html so that it becomes visible
+        // without appendchild, new element would only exist in JavaScript memory, browser would not render it
+        });
+        totalDisplay.innerText = `$${grandTotal.toFixed(2)}`;
+}
+
+//Function to update quantity
+window.changeQty = (index, amount) => {
+    menuItems[index].qty += amount;
+
+    // ensure qty never goes below 0
+    if (menuItems[index].qty < 0) menuItems[index].qty = 0
+    renderMenu(); // redraw everything
+};
+// initial draw
+renderMenu();
